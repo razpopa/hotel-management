c1e3e437cb61c05a6be862fa672a0b5e1be3d22a
Razvan <razvan.popa@accesa.eu>
2021-08-09 14:57:43 +0300

    Added functionality to Databases and Logic to BusinessLogic

---
+++ HotelApp/HotelAppLibrary/BusinessLogic/SqlProvider.cs
@@ -0,0 +1,28 @@
+﻿using HotelAppLibrary.Databases;
+using HotelAppLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelAppLibrary.BusinessLogic
+{
+	public class SqlProvider
+	{
+        private readonly ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlProvider(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+            return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",
+                                                 new { startDate, endDate },
+                                                 connectionStringName,
+                                                 true);
+        }
+
+    }
+}
---
+++ HotelApp/HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelAppLibrary.Databases
+{
+	public class SqlDataAccess : ISqlDataAccess
+	{
+		private readonly IConfiguration _config;
+
+		public SqlDataAccess(IConfiguration config)
+		{
+			_config = config;
+		}
+
+		public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionString, bool isStoredProcedure = false)
+		{
+			var connString = _config.GetConnectionString(connectionString);
+			var commandType = CommandType.Text;
+
+			if (isStoredProcedure)
+			{
+				commandType = CommandType.StoredProcedure;
+			}
+
+			using (IDbConnection connection = new SqlConnection(connString))
+			{
+				var rows = connection.Query<T>(sqlStatement, parameters, commandType: commandType).ToList();
+
+				return rows;
+			}
+		}
+
+		public void SaveData<T>(string sqlStatement, T parameters, string connectionStringName, bool isStoredProcedure = false)
+		{
+			var connString = _config.GetConnectionString(connectionStringName);
+			var commandType = CommandType.Text;
+
+			if (isStoredProcedure)
+			{
+				commandType = CommandType.StoredProcedure;
+			}
+
+			using (IDbConnection connection = new SqlConnection(connString))
+			{
+				connection.Execute(sqlStatement, parameters, commandType: commandType);
+			}
+		}
+
+	}
+}
